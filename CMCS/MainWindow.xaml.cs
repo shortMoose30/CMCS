b1d40ac226939a1792b94ea34eaa7ddc12830157
shortMoose30 <st10203732@imconnect.edu.za>
2024-10-18 15:18:58 +0200

    Add project files.

---
+++ CMCS/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace ContractClaimSystem
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmitClaim_Click(object sender, RoutedEventArgs e)
+        {
+            SubmitClaim submitClaimPage = new SubmitClaim();
+            submitClaimPage.Show();
+            this.Close();
+        }
+
+        private void BtnViewClaims_Click(object sender, RoutedEventArgs e)
+        {
+            ViewClaims viewClaimsPage = new ViewClaims();
+            viewClaimsPage.Show();
+            this.Close();
+        }
+
+        private void BtnManageContracts_Click(object sender, RoutedEventArgs e)
+        {
+            ManageContracts manageContractsPage = new ManageContracts();
+            manageContractsPage.Show();
+            this.Close();
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settingsPage = new Settings();
+            settingsPage.Show();
+            this.Close();
+        }
+    }
+}
